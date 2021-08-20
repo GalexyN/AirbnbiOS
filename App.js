b95a63626eaf84ac2ab8ed02bb62ae8dcf3b8193
GalexyN <alexneeeeewin@icloud.com>
2021-08-20 15:49:24 -0700

    successfully rendered home screen component

--- App.js
+++ App.js
@@ -1,53 +1,17 @@
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/Home';
 
-import Entype from 'react-native-vector-icons/Entypo';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <HomeScreen />
+      </SafeAreaView>
+    </>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return <SafeAreaView style={backgroundStyle}></SafeAreaView>;
-};
-
-const styles = StyleSheet.create({});
 
 export default App;
