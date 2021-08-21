168694aca5c97652dcb9bc6d72251bd94c8be155
GalexyN <alexneeeeewin@icloud.com>
2021-08-20 20:16:54 -0700

    refactored to use React stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
-
-import HomeScreen from './src/screens/Home';
-import SearchResultScreen from './src/screens/SearchResults';
-import DestinationSearchScreen from './src/screens/DestinationSearch';
-import GuestsScreen from './src/screens/Guests';
+import Router from './src/navigation/Router';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        {/* <HomeScreen /> */}
-        {/* <SearchResultScreen /> */}
-        {/* <DestinationSearchScreen /> */}
-        <GuestsScreen />
-      </SafeAreaView>
+      <Router />
     </>
   );
 };
