27219b0a418b0a57c5818bbe9b9b7dcf1205394b
GalexyN <alexneeeeewin@icloud.com>
2021-08-20 20:37:46 -0700

    successfully rendered bottom tab navigator with different screen functionality

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import DestinationSearchScreen from '../screens/DestinationSearch';
+import GuestsScreen from '../screens/Guests';
+import HomeTabNavigator from './HomeTabNavigator';
 
 const Stack = createStackNavigator();
 
@@ -10,8 +12,14 @@ const Router = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name={"Destination Search"}
-          component={DestinationSearchScreen}
+          name={'Home'}
+          component={HomeTabNavigator}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={'Guests'}
+          component={GuestsScreen}
+          options={{title: 'How many people?'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
