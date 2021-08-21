1d59f79d4c1b2b45ee46dad3e141dc7b7d6d1a46
GalexyN <alexneeeeewin@icloud.com>
2021-08-20 18:38:56 -0700

    dynamically renders data from feedjs

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import {SafeAreaView, StatusBar} from 'react-native';
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
 
+import feed from './assets/feed';
+
+const post1 = feed[0];
+
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post />
+        <Post post={post1} />
       </SafeAreaView>
     </>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -3,36 +3,31 @@ import {View, Text, Image} from 'react-native';
 
 import styles from './styles';
 
-const Post = () => {
+const Post = ({post}) => {
   return (
     <View style={styles.container}>
       {/* Image */}
       <Image
         style={styles.image}
         source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+          uri: post.image,
         }}
       />
       {/* Bed and Bedroom */}
-      <Text style={styles.bedroom}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedroom}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
       {/* Type and Description */}
       <Text style={styles.description} numberOfLines={2}>
-        Bacon ipsum dolor amet ground round salami corned beef shankle, alcatra
-        bacon chicken swine. Meatloaf jowl pork chop hamburger, frankfurter
-        short loin turducken ball tip biltong burgdoggen kevin chuck cow
-        venison. Sausage flank swine filet mignon chuck rump tongue. Hamburger
-        jerky tenderloin alcatra fatback cupim spare ribs meatloaf shoulder
-        ground round leberkas landjaeger shankle rump. Andouille pork loin shank
-        ball tip chuck prosciutto, doner landjaeger burgdoggen kevin flank strip
-        steak.
+        {post.type}. {post.title}
       </Text>
       {/* Old and New Price */}
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36</Text>
-        <Text style={styles.newPrice}> $30 </Text>/ night
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.newPrice}> ${post.newPrice} </Text>/ night
       </Text>
       {/* Total Price */}
-      <Text style={styles.totalPrice}>$230 total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} total</Text>
     </View>
   );
 };
