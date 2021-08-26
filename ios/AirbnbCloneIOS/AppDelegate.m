03ed040998ba21f6c20501dd46b5048a748a665e
Alexander Nguyen <66571090+GalexyN@users.noreply.github.com>
2021-08-25 18:22:48 -0700

    Update AppDelegate.m

--- ios/AirbnbCloneIOS/AppDelegate.m
+++ ios/AirbnbCloneIOS/AppDelegate.m
@@ -30,7 +30,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyB31bc4k27-tI4V9myliEDB3mB20oyQx2M"]; // add this line using the api key obtained from Google Console
+  [GMSServices provideAPIKey:@""]; // add this line using the api key obtained from Google Console
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
